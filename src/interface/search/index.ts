e0a35e0c8fa57ba2c4d665869abcc77a9be0fa1c
Eric <54091924+EricHsia7@users.noreply.github.com>
2026-05-27 23:32:07 +0800

    update

--- src/interface/search/index.ts
+++ src/interface/search/index.ts
@@ -1,6 +1,9 @@
+import { searchFor } from '../../data/search';
+
 const searchField = document.querySelector('.css_search_field') as HTMLElement;
 const searchHeadElement = searchField.querySelector('.css_search_head') as HTMLElement;
 const searchHeadLeftButtonElement = searchHeadElement.querySelector('.css_search_head_button_left') as HTMLElement;
+const searchHeadSearchInputElement = searchHeadElement.querySelector('.css_search_head_search_input input[type="text"]') as HTMLInputElement;
 
 let initialized: boolean = false;
 
@@ -13,6 +16,26 @@ export async function initializeSearchField() {
   searchHeadLeftButtonElement.onclick = function () {
     closeSearch();
   };
+
+  searchHeadSearchInputElement.addEventListener('selectionchange', function () {
+    updateSearchResults();
+  });
+
+  searchHeadSearchInputElement.addEventListener('keyup', function () {
+    updateSearchResults();
+  });
+
+  searchHeadSearchInputElement.addEventListener('paste', function () {
+    updateSearchResults();
+  });
+
+  searchHeadSearchInputElement.addEventListener('cut', function () {
+    updateSearchResults();
+  });
+}
+
+function updateSearchResults(): void {
+  console.log(searchFor(searchHeadSearchInputElement.value));
 }
 
 export function showSearch(): void {
